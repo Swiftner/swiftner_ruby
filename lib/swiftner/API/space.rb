f8f26c469ebc0fa28b2ce9ac5ebf388f4cf880d5
FarouqJalabi <farouqjalabi@gmail.com>
2024-09-09 15:01:02 +0200

    Added YARD comments for services

--- lib/swiftner/API/space.rb
+++ lib/swiftner/API/space.rb
@@ -6,16 +6,26 @@ module Swiftner
     # Inherits from the Service class.
     # Provides methods for interacting with spaces.
     class Space < Service
+      # Finds all spaces
+      # @return [Array<Swiftner::API::Space>]
       def self.find_spaces
         response = client.get("/space/get-spaces")
         map_collection(response)
       end
 
+      # Finds space by id
+      # @param [Integer] space_id
+      # @return [Swiftner::API::Space]
       def self.find(space_id)
         response = client.get("/space/get/#{space_id}")
         build(response.parsed_response)
       end
 
+      # Creates a space.
+      # @param [Hash] attributes
+      # @option attributes [String] :name (required)
+      # @option attributes [String] :description (required)
+      # @option attributes [Integer] :prompt_id (optional)
       def self.create(attributes)
         validate_required(attributes, :name, :description)
 
@@ -28,6 +38,12 @@ module Swiftner
         build(response.parsed_response)
       end
 
+      # Updates space
+      # @param [Hash] attributes
+      # @option attributes [String] :name (optional)
+      # @option attributes [String] :description (optional)
+      # @option attributes [Integer] :prompt_id (optional)
+      # @return [Swiftner::API::Space]
       def update(attributes)
         attributes = attributes.transform_keys(&:to_s)
         @details = @details.merge(attributes)
@@ -42,6 +58,8 @@ module Swiftner
         self
       end
 
+      # Deletes space
+      # @return [Hash]
       def delete
         client.delete("/space/delete/#{id}")
       end
