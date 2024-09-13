b5be6c0e7b3593429ba7efc645635918b807ab31
FarouqJalabi <farouqjalabi@gmail.com>
2024-09-13 10:49:44 +0200

    Fetching accepteded file types

--- lib/swiftner/configuration.rb
+++ lib/swiftner/configuration.rb
@@ -4,10 +4,30 @@ module Swiftner
   ## `Swiftner::Configuration`
   class Configuration
     SUPPORTED_LANGUAGES = %w[en no].freeze
-    attr_accessor :client
+    attr_reader :client
 
     def initialize
       @client = nil
     end
+
+    def client=(client)
+      @client = client
+      @accepted_file_types = fetch_accepted_file_types
+    end
+
+    def fetch_accepted_file_types
+      # ! /upload/accepted_file_types gives 401 Unauthorized with good API key
+      # ! should be fixed soon
+      # response = @client.get("/upload/accepted_file_types")
+      # @accepted_file_types = handle_response(response).parsed_response
+      # rescue Unauthorized => e
+      #   puts "Authentication failed. Please check your API key."
+      #   raise e
+      @accepted_file_types = {
+        video: %w[mp4 mkv mov avi flv wmv webm],
+        audio: %w[mp3 wav wma m4v]
+      }
+    end
+
   end
 end
